c96fa2148ae17824f4b515f6ec903be1b1b5d880
thedtj <awesomedan@gmail.com>
2019-10-07 12:23:55 -0700

    Another Try

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
-require = require('"esm')(module)
+// import express from 'express'
+// import { getHTML, getDates, getShowtimes } from './lib/scraper'
+// import './lib/cron'
 
-module.exports = require('./main.js')
+const express = require('express')
+const getHTML = require('./lib/scraper.js')
+const getDates = require('./lib/scraper.js')
+const getShowtimes = require('./lib/scraper.js')
+const cron = require('./lib/cron.js')
+
+const app = express()
+
+// eslint-disable-next-line no-unused-vars
+app.get('/', async (req, res) => {
+
+	const showtimesHTML = await getHTML(
+		'http://ellensburgmovies.com/gmc_html/gmc_html_showtimes.html'
+	)
+
+	const [dates, { movies, comingSoon }] = await Promise.all([
+		getDates(showtimesHTML),
+		getShowtimes(showtimesHTML)
+	])
+
+	res.json({ dates, movies, comingSoon })
+})
+
+const PORT = process.eventNames.PORT || 5800
+
+app.listen(PORT, () => console.log(`app running on port ${PORT}`))
+
+// export default app
